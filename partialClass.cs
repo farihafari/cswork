eee560a7a88ac25bd16007cd633ae2b9259d16a6
farihafari <farihafari594@gmail.com>
2024-01-18 15:06:29 +0500

    generic work

---
+++ partialClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClass
+{
+    internal partial class MyClassDetails
+    {
+        string data;
+        public MyClassDetails() {
+
+            data = "this is first partial class";
+        }
+    }
+    
+}
